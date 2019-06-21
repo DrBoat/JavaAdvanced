c3eb083b204e575b6db3520d82dbc73b97a473da
Vozleev Yury <vozleeff@gmail.com>
2019-06-21 14:07:14 +0300

    return concurrent list

---
+++ Vozleev-exam2/src/ru/ifmo/rain/vozleev/Cockroach.java
@@ -0,0 +1,11 @@
+package ru.ifmo.rain.vozleev;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
+public class Cockroach {
+    public void run(List<Integer> list) {
+        Arrays.sort(list.toArray());
+    }
+}
