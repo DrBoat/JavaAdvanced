eb8e59b5b13d5e0f3a7ea065a577215c860bdd68
Vozleev Yury <vozleeff@gmail.com>
2019-06-21 13:33:40 +0300

    in process

--- Vozleev-exam2/src/ru/ifmo/rain/vozleev/Сockroach.java
+++ Vozleev-exam2/src/ru/ifmo/rain/vozleev/Сockroach.java
@@ -1,6 +1,15 @@
 package ru.ifmo.rain.vozleev;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Сockroach {
+
+    public void sortArray(ArrayList<Integer> arrayForSort)
+    {
+
+    }
+
     //принял массив
     //отсортил
     //отчитался, что отсортил
